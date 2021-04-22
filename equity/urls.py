914947c0a91317f7de081fc59fd8dff607e9902e
amartya <amartyathorat@gmail.com>
2021-04-22 13:05:20 +0530

    format urls

--- equity/urls.py
+++ equity/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     #latest update result
-    path('equity/bse/', views.EquitiesView.as_view()),
-    path('equity/bse/search',views.EquitiesView.as_view()),
+    path('equity/', views.EquitiesView.as_view()),
+    path('equity/search/',views.EquitiesView.as_view()),
     #date specified result
-    path('equity/bse/date/<slug:date>/', views.EquitiesView.as_view()),
-    path('equity/bse/date/<slug:date>/search', views.EquitiesView.as_view()),
+    path('equity/date/<slug:date>/', views.EquitiesView.as_view()),
+    path('equity/date/<slug:date>/search/', views.EquitiesView.as_view()),
 ]
