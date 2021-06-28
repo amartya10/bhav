57c87c045cd8b971a5d35a9f995f4afe36625ab3
amartya <amartyathorat@gmail.com>
2021-06-28 20:15:53 +0530

    fix url mapping

--- equity/urls.py
+++ equity/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     #latest update result
-    path('equity', views.EquitiesView.as_view()),
+    path('equity/', views.EquitiesView.as_view()),
     path('equity/search/',views.EquitiesView.as_view()),
     #date specified result
     path('equity/date/<slug:date>', views.EquitiesView.as_view()),
